de087d2b835eb39ce1aedee0eb5a5ccf8795fa85
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2025-02-20 00:37:15 +0300

    feat: shuffle

--- deck/deck.go
+++ deck/deck.go
@@ -3,6 +3,8 @@ package deck
 import (
 	"fmt"
 	"strconv"
+
+	"math/rand"
 )
 
 type Suit int
@@ -69,6 +71,16 @@ func New() Deck {
 		}
 	}
 
+	return shuffle(d)
+}
+
+func shuffle(d Deck) Deck {
+	for i := 0; i < len(d); i++ {
+		r := rand.Intn(i + 1)
+		if r != i {
+			d[i], d[r] = d[r], d[i]
+		}
+	}
 	return d
 }
 
